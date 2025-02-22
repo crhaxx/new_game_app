d0235c9c9d54d13f424d63cc18e808f2135a645a
developer-sogyy <opekanyhermelin@gmail.com>
2025-02-22 13:02:39 +0100

    Added Readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:new_game_app/database/auth/auth_gate.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env');
   await Supabase.initialize(
-      anonKey:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdqc2pyY2JzaGdrYXZpd2R0eG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNzc4NzMsImV4cCI6MjA1NTY1Mzg3M30._LYAarCghY5e3WCD5O8CSZYUpY13OaE5MY1qdE5CO9M",
-      url: "https://gjsjrcbshgkaviwdtxnn.supabase.co");
+      anonKey: dotenv.env['anonKey'] ?? "", url: dotenv.env['url'] ?? "");
   runApp(const MainApp());
 }
 
